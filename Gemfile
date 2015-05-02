82b9b353abcec2a55d9e6bc223de2e13466d0802
askl56 <amscott@openmailbox.org>
2015-05-02 03:47:09 +0100

    readded regression testing, fixed folder structure

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
+gem 'groupdate'
 
 group :development, :test do
   gem 'byebug'
---
+++ spec/models/regression/event_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe Event, regressor: true do
+
+  # === Relations ===
+  it { is_expected.to belong_to :registered_application}
+  
+  
+
+  # === Nested Attributes ===
+  
+
+  # === Database (Columns) ===
+  it { is_expected.to have_db_column :id }
+	it { is_expected.to have_db_column :name }
+	it { is_expected.to have_db_column :registered_application_id }
+	it { is_expected.to have_db_column :created_at }
+	it { is_expected.to have_db_column :updated_at }
+
+  # === Database (Indexes) ===
+  it { is_expected.to have_db_index ["registered_application_id"]}
+
+  # === Validations (Length) ===
+  
+
+  # === Validations (Presence) ===
+  it { is_expected.to validate_presence_of :name }
+
+  # === Validations (Numericality) ===
+  
+
+  
+  # === Enums ===
+  
+  
+end
