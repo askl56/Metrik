11df51d352e535317f42ef3009ad4cbfaaef6a25
askl56 <amscott@openmailbox.org>
2015-05-03 17:59:51 +0100

    fixed code coverage issues and hopefully fixed travis problems

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,8 +8,6 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'shoulda/matchers'
 require "pundit/rspec"
-require 'coveralls'
-Coveralls.wear!
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
