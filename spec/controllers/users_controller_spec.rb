e948f07b8f6d5fa6e0a1c2094e75e705bb869e36
askl56 <amscott@openmailbox.org>
2015-06-01 01:37:56 +0100

    fixing rubocop errors

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 describe UsersController, type: :controller do
   describe "GET Index" do
     context 'with valid credentials' do
+      it "has a 200 status code" do
+        sign_in
+        get :index
+        expect(response.status).to eq(200)
+      end
     end
   end
 end
--- spec/views/users/index.html.haml_spec.rb
+++ spec/views/users/index.html.haml_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-describe "users/index.html.haml" do
+describe 'users/index.html.haml' do
   include Devise::TestHelpers
-  context "when there are 2 users" do
+  context 'when there are 2 users' do
     before(:each) do
       assign(:users, [
-               stub_model(User, :email => "1@example.com"),
-               stub_model(User, :email => "2@example.com")
+               stub_model(User, :email => '1@example.com'),
+               stub_model(User, :email => '2@example.com')
       ])
     end
 
-    it "displays both users" do
+    it 'displays both users' do
       render
-      expect(rendered).to include("1@example.com")
-      expect(rendered).to include("2@example.com")
+      expect(rendered).to include('1@example.com')
+      expect(rendered).to include('2@example.com')
     end
   end
 end
