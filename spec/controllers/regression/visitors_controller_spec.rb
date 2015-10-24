3ff3bdfa843e9d99ce4008462972df28b3e1b0cc
askl56 <amscott@openmailbox.org>
2015-10-24 10:51:23 +0100

    tests updated

--- spec/controllers/regression/visitors_controller_spec.rb
+++ spec/controllers/regression/visitors_controller_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe VisitorsController, regressor: true do
   # === Routes (REST) ===
 
   # === Callbacks (Before) ===
-  it { should use_before_filter(:verify_authenticity_token) }
-  it { should use_before_filter(:set_xhr_redirected_to) }
-  it { should use_before_filter(:set_request_method_cookie) }
+  it { is_expected.to use_before_filter(:verify_authenticity_token) }
+  it { is_expected.to use_before_filter(:set_xhr_redirected_to) }
+  it { is_expected.to use_before_filter(:set_request_method_cookie) }
   # === Callbacks (After) ===
-  it { should use_after_filter(:verify_same_origin_request) }
-  it { should use_after_filter(:abort_xdomain_redirect) }
+  it { is_expected.to use_after_filter(:verify_same_origin_request) }
+  it { is_expected.to use_after_filter(:abort_xdomain_redirect) }
   # === Callbacks (Around) ===
 end
